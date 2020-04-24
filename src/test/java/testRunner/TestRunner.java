e858a7e909cc45144640bb0add489610387f5ef4
viral patel <vir@qable.io>
2020-04-24 12:38:28 +0530

    Initial Commit

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "E:/demo/Cucumber/CucumberDemo/Features/Demo.feature",
+        features = "C:\\Users\\viral\\IdeaProjects\\CucumberDemo\\Features\\1.feature",
         glue="stepDefinition",
         plugin = { "pretty", "json:target/Cucumber.json" },
         monochrome = true
