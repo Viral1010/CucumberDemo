ab2ea5d02287aee9e351514d816394bc93bfd762
viral patel <vir@qable.io>
2020-04-25 15:55:46 +0530

    Initial Commit

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "/Features/temp.feature",
+        features = "C:\\Program Files (x86)\\Jenkins\\workspace\\Xray_Testing\\Features\\temp.feature",
         glue="stepDefinition",
         plugin = { "pretty", "json:target/Cucumber.json" },
         monochrome = true
